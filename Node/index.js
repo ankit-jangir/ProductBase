eb56cba41335e2b78344e3a8787705c618a5fa0c
ankit-jangir <ankitjangir5824@gmail.com>
2025-08-21 10:44:38 +0530

    main changes

--- Node/index.js
+++ Node/index.js
@@ -2,12 +2,21 @@ const express = require('express');
 const Router = require('./Src/Router/Router');
 const app = express();
 const path = require("path");
+const cors = require("cors");  
+
 app.use(express.json());
+app.use(cors({
+    origin: "http://localhost:5173", 
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
+
 const port = 7777;
 require('./Src/confic/confic');
 app.use(Router);
+
 app.use("/uploads", express.static(path.join(__dirname, "public", "uploads")));
 
 app.listen(port, () => {
     console.log(`server is connected ${port}`);
-})
+});
